f552dfca6c0f3125eb1360c99f63953ec952f0ae
David Goehrig <dave@dloh.org>
2014-02-10 12:20:44 -0500

    Initial import of the mvn project
    Need to fixup the default crap app stuff

---
+++ src/main/java/org/dloh/luce/App.java
@@ -0,0 +1,13 @@
+package org.dloh.luce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
